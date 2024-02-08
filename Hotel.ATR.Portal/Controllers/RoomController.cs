38f0029c47304c1d0eafba33ee58922b0d16f241
Olzhas Adylkhanov <adylkh4nov@gmail.com>
2024-02-08 09:09:40 +0600

    initial commit

---
+++ Hotel.ATR.Portal/Controllers/RoomController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Hotel.ATR.Portal.Controllers
+{
+    public class RoomController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult RoomList()
+        {
+            return View();
+        }
+
+        public IActionResult RoomDetails()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CreateSubscribe(string email)
+        {
+            return RedirectToAction("RoomDetails");
+        }
+    }
+}
---
+++ Hotel.ATR.Portal/Views/Shared/_mainMenu.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Hotel.ATR.Portal.Views.Shared
+{
+    public class _mainMenuModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
